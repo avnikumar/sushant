63e2483c276b0a5e656c30569584b7e838a7a778
singh3547 <avni.kumar1987@gmail.com>
2025-07-05 21:55:21 +0530

    auto execute composer file

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php echo '<h1>Hello World</h1>'; ?>
-<?php echo '<h2>This is avnish testing for CI-CD Elastic benastalk</h2>'; ?>
+<?php echo '<h2>This is avnish singh testing for CI-CD Elastic benastalk</h2>'; ?>
 
