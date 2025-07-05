78be2cf9561e1465312f2faebce6455ba59d3c5c
singh3547 <avni.kumar1987@gmail.com>
2025-07-05 21:18:20 +0530

    second commit

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php echo '<h1>Hello World</h1>'; ?>
 <?php echo '<h2>This is avnish testing for CI-CD Elastic benastalk</h2>'; ?>
-<?php echo '<h1>Hello Sushant</h1>'; ?>
-<?php echo '<h2>How is your handwriting?</h2>'; ?>
 
